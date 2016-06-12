cb312e9d3d4fc1acf3041f3b51426a6366023ef5
Meadowcottage <bendixon50@gmail.com>
2016-06-12 17:43:43 +0100

    Added initial Winstaller dependency

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const electronInstaller = require('electron-winstaller');
 
 let mainWindow
 
@@ -39,3 +40,29 @@ app.on('activate', function () {
     createWindow()
   }
 })
+
+resultPromise = electronInstaller.createWindowsInstaller({
+    appDirectory: '../build/devrant.io-win32-x64',
+    outputDirectory: '../package',
+    authors: 'devRant.io',
+    exe: 'devRant.io.exe'
+
+    appDirectory: '../build/devrant.io-win32-x64',
+    authors: 'devRant.io Team | [MEADOW_DEV]',
+    exe: 'devRant.io.exe',
+    iconUrl: '../icon/favicon.ico',
+    loadingGif: path.join(config.STATIC_PATH, 'loading.gif'),
+    name: config.APP_NAME,
+    noMsi: true,
+    outputDirectory: DIST_PATH,
+    productName: config.APP_NAME,
+    remoteReleases: config.GITHUB_URL,
+    setupExe: config.APP_NAME + 'Setup-v' + config.APP_VERSION + '.exe',
+    setupIcon: config.APP_ICON + '.ico',
+    signWithParams: signWithParams,
+    title: config.APP_NAME,
+    usePackageJson: false,
+    version: pkg.version
+  });
+
+resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
