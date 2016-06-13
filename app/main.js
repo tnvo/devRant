143996c90cf375b95873576820f419518eed49be
Meadowcottage <bendixon50@gmail.com>
2016-06-13 11:37:05 +0100

    Added Winstaller | Removed Electron Packager

--- app/main.js
+++ app/main.js
@@ -1,10 +1,12 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+//var electronInstaller = require('electron-winstaller');
 
 const app_name = 'devRant.io';
 const app_title = 'devRant // new Rant("fml");';
 const app_version = '1.0.1';
+const app_description = 'The unofficial electron app for devRant.io';
 
 let mainWindow
 
@@ -41,3 +43,19 @@ function createWindow () {
     createWindow()
     }
   })
+
+  /**resultPromise = electronInstaller.createWindowsInstaller({
+    appDirectory: '.',
+    outputDirectory: '/dist/win',
+    loadingGif: '/build/loadingGif.gif',
+    authors: app_name + ' Team | [MEADOW_DEV]',
+    exe: app_name + '.exe',
+    description: app_description,
+    version: app_version,
+    title: app_title,
+    iconUrl: 'https://raw.githubusercontent.com/Meadowcottage/Devrant.io/master/icon/favicon.ico',
+    setupIcon: 'https://raw.githubusercontent.com/Meadowcottage/Devrant.io/master/icon/favicon.ico',
+    setupExe: app_name + '-' + app_version + '-setup.exe',
+    noMsi: true,
+    remoteReleases: 'https://github.com/Meadowcottage/Devrant.io'
+  });**/
