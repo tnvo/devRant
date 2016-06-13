e1c95bcde8a6670b177a87019a8db5caa0dd362a
Meadowcottage <bendixon50@gmail.com>
2016-06-13 10:17:58 +0100

    Removed Winstaller(TEMP)

--- app/main.js
+++ app/main.js
@@ -1,7 +1,6 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const electronInstaller = require('electron-winstaller');
 
 const app_name = 'devRant.io';
 const app_title = 'devRant // new Rant("fml");';
@@ -42,21 +41,3 @@ function createWindow () {
     createWindow()
     }
   })
-
-resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: '../build/devrant.io-win32-x64',
-    outputDirectory: '../package/devrant.io-win32-x64',
-    name: app_name,
-    title: app_name,
-    version: app_version,
-    productName: app_name,
-    authors: app_name + ' Team | [MEADOW_DEV]',
-    exe: app_name + '.exe',
-    iconUrl: '../icon/favicon.ico',
-    loadingGif: '../icon/Installing.gif',
-    noMsi: false,
-    setupExe: app_name + '-setup.exe',
-    setupIcon: '../favicon.ico'
-  });
-
-resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
