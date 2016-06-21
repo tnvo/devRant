8911b8d7c837132cfb85245242944fc716103d18
Meadowcottage <bendixon50@gmail.com>
2016-06-21 02:08:55 +0100

    Initial Menu Items/Keybindings commit

--- app/main.js
+++ app/main.js
@@ -1,18 +1,56 @@
 const electron = require('electron');
 const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
 const app_name = 'devRant.io';
 const app_title = 'devRant // new Rant("fml");';
 const app_version = '1.0.1';
 const app_description = 'The unofficial electron app for devRant.io';
+const app_menu = electron.Menu;
+const BrowserWindow = electron.BrowserWindow;
+
+const menu_content = [
+  {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CommandOrControl+Z', role: 'undo' },
+      { label: 'Redo', accelerator: 'Shift+CommandOrControl+Z', role: 'redo' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CommandOrControl+X', role: 'cut' },
+      { label: 'Copy', accelerator: 'CommandOrControl+C', role: 'copy' },
+      { label: 'Paste', accelerator: 'CommandOrControl+V', role: 'paste' },
+      { label: 'Select All', accelerator: 'CommandOrControl+A', role: 'selectall' }
+    ]
+  }, {
+    label: 'View',
+    submenu: [
+      { label: 'Reload', accelerator: 'CommandOrControl+R', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.reload();} }
+    ]
+  }, {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      { label: 'Minimize', accelerator: 'CommandOrControl+M', role: 'minimize' },
+      { label: 'Close', accelerator: 'CommandOrControl+W', role: 'close' }
+    ]
+  }
+];
 
 let mainWindow
 
 app.on('ready', function createWindow () {
-  mainWindow = new BrowserWindow({title: app_title, movable: true, width: 450, height: 912, minWidth: 450, minHeight: 500, maxWidth: 640, fullscreenable: false, resizable: true, autoHideMenuBar: true})
+  mainWindow = new BrowserWindow({
+    title: app_title,
+    movable: true,
+    width: 450,
+    height: 912,
+    minWidth: 450,
+    minHeight: 500,
+    maxWidth: 640,
+    fullscreenable: false,
+    resizable: true,
+    autoHideMenuBar: true
+  })
+  app_menu.setApplicationMenu(app_menu.buildFromTemplate(menu_content));
   mainWindow.loadURL('file://' + __dirname + '/index.html')
-  mainWindow.setMenu(null)
   mainWindow.on('closed', function () {
   mainWindow = null
   })
