d840779f18d94945d2c4a9a002cba2661265769d
Meadowcottage <bendixon50@gmail.com>
2016-06-22 17:34:44 +0100

    Added 'Help' section to menubar

--- app/main.js
+++ app/main.js
@@ -31,6 +31,15 @@ const menu_content = [
       { label: 'Minimize', accelerator: 'CommandOrControl+M', role: 'minimize' },
       { label: 'Close', accelerator: 'CommandOrControl+W', role: 'close' }
     ]
+  }, {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      { label: 'About devRant', click: function() { require('electron').shell.openExternal("https://github.com/Meadowcottage/devRant.io" + app_version) } },
+      { label: 'View devRant', click: function() { require('electron').shell.openExternal("https://devrant.io") } },
+      { type: 'separator' },
+      { label: 'Changelog', click: function() { require('electron').shell.openExternal("https://github.com/Meadowcottage/devRant.io/releases/tag/" + app_version) } }
+    ]
   }
 ];
 
