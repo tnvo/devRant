c43ac288eea2fd0e3d8e1dc934b8134937e85832
[MEADOW_DEV} <bendixon50@gmail.com>
2016-06-25 01:58:34 +0100

    TEMP Added custom menu's to Darwin
    Need to fix to not have duplicate menu code.

--- app/main.js
+++ app/main.js
@@ -16,7 +16,7 @@ const BrowserWindow = electron.BrowserWindow;
 const GhReleases = require('electron-gh-releases');
 
 //Menu Contents for MacOS ONLY
-const macOS_menu_content = [
+const darwin_menu_content = [
   {
     label: 'Application',
     submenu: [
@@ -25,8 +25,43 @@ const macOS_menu_content = [
       { type: 'separator' },
       { label: 'Quit', accelerator: 'Command+Q', click: function() {app.quit();} }
     ]
-  },
-]
+  }, {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CommandOrControl+Z', role: 'undo' },
+      { label: 'Redo', accelerator: 'Shift+CommandOrControl+Z', role: 'redo' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CommandOrControl+X', role: 'cut' },
+      { label: 'Copy', accelerator: 'CommandOrControl+C', role: 'copy' },
+      { label: 'Paste', accelerator: 'CommandOrControl+V', role: 'paste' },
+      { label: 'Select All', accelerator: 'CommandOrControl+A', role: 'selectall' }
+    ]
+  }, {
+    label: 'View',
+    submenu: [
+      { label: 'Forward', accelerator: 'CommandOrControl+Right', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.goForward();console.log("Going forward a page!")} },
+      { label: 'Back', accelerator: 'CommandOrControl+Left', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.goBack();console.log("Going back a page!")} },
+      { type: 'separator' },
+      { label: 'Reload', accelerator: 'CommandOrControl+R', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.reload();} }
+    ]
+  }, {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      { label: 'Minimize', accelerator: 'CommandOrControl+M', role: 'minimize' },
+      { label: 'Close', accelerator: 'CommandOrControl+W', role: 'close' }
+    ]
+  }, {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      { label: 'About devRant', click: function() { require('electron').shell.openExternal("https://github.com/Meadowcottage/devRant.io" + app_version) } },
+      { label: 'View devRant', click: function() { require('electron').shell.openExternal("https://devrant.io") } },
+      { type: 'separator' },
+      { label: 'Changelog', click: function() { require('electron').shell.openExternal("https://github.com/Meadowcottage/devRant.io/releases/tag/" + app_version) } }
+    ]
+  }
+];
 
 //General Menu Contents
 const menu_content = [
@@ -71,6 +106,9 @@ const menu_content = [
 // Main App Window
 let mainWindow
 
+// Chooses menu to load based on OS
+var app_OS_menu;
+
 // Auto Update Options
 let options = {
   repo: 'meadowcottage/devRant.io',
@@ -103,6 +141,15 @@ app.on('will-finish-launching', function createWindow () {
   updater.autoUpdater
 })
 
+// If OS is Darwn(MacOS)
+if (process.platform == 'darwin') {
+  app_titleBarStyle = 'hidden-inset';
+  app_OS_menu = darwin_menu_content;
+} else {
+  app_titleBarStyle = 'default';
+  app_OS_menu = menu_content;
+}
+
 app.on('ready', function createWindow () {
   mainWindow = new BrowserWindow({
     title: app_title,
@@ -116,11 +163,7 @@ app.on('ready', function createWindow () {
     resizable: true,
     autoHideMenuBar: true
   })
-  if (process.platform == 'darwin') {
-    app_menu.setApplicationMenu(app_menu.buildFromTemplate(macOS_menu_content + menu_content));
-  } else {
-    app_menu.setApplicationMenu(app_menu.buildFromTemplate(menu_content));
-  }
+  app_menu.setApplicationMenu(app_menu.buildFromTemplate(app_OS_menu))
   mainWindow.loadURL('file://' + __dirname + '/index.html')
   mainWindow.on('closed', function () {
   mainWindow = null
