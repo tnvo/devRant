9f56b84149be9468d0d93076ec0016517168ba50
[MEADOW_DEV} <bendixon50@gmail.com>
2016-07-07 04:29:55 +0100

    TEMP fix for platform menus

--- app/main.js
+++ app/main.js
@@ -31,7 +31,7 @@ const menu_content = [
   }, {
     label: 'View',
     submenu: [
-      { label: 'Forward', accelerator: 'CommandOrControl+Right', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.goForward();} },
+      //{ label: 'Forward', accelerator: 'CommandOrControl+Right', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.goForward();} },
       { label: 'Back', accelerator: 'CommandOrControl+Left', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.goBack();focusedWindow.webContents.reload()} },
       { type: 'separator' },
       { label: 'Reload', accelerator: 'CommandOrControl+R', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.reload();} }
@@ -65,7 +65,42 @@ const darwin_menu_content = [
       { type: 'separator' },
       { label: 'Quit', accelerator: 'Command+Q', click: function() {app.quit();} }
     ]
-  }, menu_content
+  }, {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CommandOrControl+Z', role: 'undo' },
+      { label: 'Redo', accelerator: 'Shift+CommandOrControl+Z', role: 'redo' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CommandOrControl+X', role: 'cut' },
+      { label: 'Copy', accelerator: 'CommandOrControl+C', role: 'copy' },
+      { label: 'Paste', accelerator: 'CommandOrControl+V', role: 'paste' },
+      { label: 'Select All', accelerator: 'CommandOrControl+A', role: 'selectall' }
+    ]
+  }, {
+    label: 'View',
+    submenu: [
+      //{ label: 'Forward', accelerator: 'CommandOrControl+Right', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.goForward(; focusedWindow.webContents.reload();} },
+      { label: 'Back', accelerator: 'CommandOrControl+Left', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.goBack(); focusedWindow.webContents.reload();} },
+      { type: 'separator' },
+      { label: 'Reload', accelerator: 'CommandOrControl+R', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.reload();} }
+    ]
+  }, {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      { label: 'Minimize', accelerator: 'CommandOrControl+M', role: 'minimize' },
+      { label: 'Close', accelerator: 'CommandOrControl+W', role: 'close' }
+    ]
+  }, {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      { label: 'About devRant', click: function() { require('electron').shell.openExternal("https://github.com/Meadowcottage/devRant.io" + app_version) } },
+      { label: 'View devRant', click: function() { require('electron').shell.openExternal("https://devrant.io") } },
+      { type: 'separator' },
+      { label: 'Changelog', click: function() { require('electron').shell.openExternal("https://github.com/Meadowcottage/devRant.io/releases/tag/" + app_version) } }
+    ]
+  }
 ];
 
 // Main App Window
