774c87aad6a6708ac0035b1a69ef2405290cd27e
[MEADOW_DEV] <bendixon50@gmail.com>
2016-08-21 01:04:52 +0100

    Moved from WebView to WebContents

---
+++ app/config.js
@@ -0,0 +1,10 @@
+const config = require('electron-config');
+
+module.exports = new config({
+    defaults: {
+        lastWindowState: {
+            width: 450,
+            height: 912
+        }
+    }
+});
--- app/main.js
+++ app/main.js
@@ -10,15 +10,27 @@ const app_name = app.getName();
 const app_title = app.getName() + ' // new Rant("fml");';
 const app_version = app.getVersion();
 const app_description = 'The unofficial electron app for devRant';
+const app_config = require('./config');
+const app_is_dev = require('electron-is-dev');
+
+// System paths
+const path = require('path');
+const fs = require('fs');
 
 // Main App Window
 let mainWindow
 
-app.on('ready', function createWindow() {
-    mainWindow = new browserWindow({
+// Main Window
+function createMainWindow() {
+    const lastWindowState = app_config.get('lastWindowState');
+    const app_view = new electron.BrowserWindow({
         title: app_title,
         titleBarStyle: 'hidden-inset',
         backgroundColor: '#40415a',
+        x: lastWindowState.x,
+        y: lastWindowState.y,
+        width: lastWindowState.width,
+        height: lastWindowState.height,
         movable: true,
         width: 450,
         height: 912,
@@ -27,28 +39,76 @@ app.on('ready', function createWindow() {
         maxWidth: 640,
         fullscreenable: false,
         resizable: true,
-        autoHideMenuBar: true
-    })
-    menu.setApplicationMenu(require('./menu.js'));
-    mainWindow.loadURL('file://' + __dirname + '/index.html')
-    mainWindow.on('closed', function() {
-        mainWindow = null
-    })
-
-    //Shortcut
-    globalShortcut.register('Control+R', () => {
-        mainWindow.webContents.reload();
-    })
-    globalShortcut.register('CmdOrCtrl+Left', () => {
-        mainWindow.webContents.goBack();
-        mainWindow.webContents.reload();
-    })
-
-    mainWindow.on('app-command', (e, cmd) => {
-        // Navigate the window back when the user hits their mouse back button
-        if (cmd === 'browser-backward' && mainWindow.webContents.canGoBack()) {
-            mainWindow.webContents.goBack()
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: false,
+            plugins: true
         }
+    });
+    app_view.loadURL('https://devrant.io/feed');
+    return app_view;
+}
+
+app.on('ready', function createWindow() {
+    mainWindow = createMainWindow();
+    menu.setApplicationMenu(require('./menu'))
+    if (app_is_dev) { mainWindow.openDevTools() }
+
+    const app_page = mainWindow.webContents;
+
+    app_page.on('dom-ready', () => {
+
+              // Stock style additions
+              app_page.insertCSS(fs.readFileSync(path.join(__dirname, 'styles/app.css'), 'utf8'));
+
+              // MacOS Button Offset & Navbar Padding
+              if (process.platform == 'darwin') {
+                app_page.insertCSS('.addrant-btn, .addcomment-btn{bottom: 44px!important;} .rant-top-bar { padding-top: 20px!important; }');
+              } else {
+                app_page.insertCSS('.addrant-btn, .addcomment-btn{bottom: 20px!important;}');
+              }
+
+              //Extenal Links Open in New Window
+              app_page.executeJavaScript("$('a').not('[href*=\"mailto:\"]').each(function (){var isInternalLink = new RegExp('/' + window.location.host + '/');if ( ! isInternalLink.test(this.href) ) {$(this).attr('target', '_blank');}});");
+
+              // Adds Download Button to Menu
+              app_page.executeJavaScript("$('<li><a target=\"_blank\" href=\"https://www.devrant.io/\"><span class=\"icon-about2 icon\"></span>Downloads</a></li>').insertAfter('div.menu-modal > ul > li:nth-child(5)')");
+
+              // Adds Feedback Button to Menu
+              app_page.executeJavaScript("$('<li><a target=\"_blank\" href=\"mailto:info@devrant.io\"><span class=\"icon-feedback2 icon\"></span>Feedback</a></li>').insertAfter('div.menu-modal > ul > li:nth-child(5)')");
+
+              //Adds Back Button
+              app_page.executeJavaScript("$('.feed-top-icons:nth-child(2), .rant-top-bar > .share-icons, div.body-col2.profile-page > .rant-top-bar').prepend('<a href=\"javascript: history.back();\" title=\"Back\" alt=\"Back\"><span class=\"icon-back2 icon\"></span></a>')");
+
+              // Toggle Theme
+              app_page.executeJavaScript("function addDarkTheme(){$('head').prepend('<style id=\"dark-theme\">body, .rantlist li, .profile-details, .profile-tabs, .rantlist-tags a, .rantlist-tags a, .search-base-list a{background-color: #414159!important;} .rant-top-bar, .tab-line, .reply-bar{background-color: #545571!important;} .rantlist li, .search-base-list a{border-top: 2px solid #54556E!important;} .profile-tabs{border-bottom: 2px solid #54556E!important;} div.username-row > a.username-details, .rantlist-title, .profile-detail-col2, .tabs, .profile-detail-col2 > a, .search-popular{color: #E0E0E0!important;} .rantlist-tags a{border: 2px solid #54556E!important;}</style>');}");
+              app_page.executeJavaScript("function removeDarkTheme(){$('head > #dark-theme').remove();}");
+              app_page.executeJavaScript("$('<li><a href=\"#\" onClick=\"addDarkTheme()\">Dark Theme</a></li>').prependTo('div.settings-modal.modal-base > ul')");
+              app_page.executeJavaScript("$('<li><a href=\"#\" onClick=\"removeDarkTheme()\">Light Theme</a></li>').prependTo('div.settings-modal.modal-base > ul')");
+
+              mainWindow.show();
+
+              //Open external links in browser
+              app_page.on('new-window', (e, url) => {
+                  e.preventDefault();
+                  electron.shell.openExternal(url);
+              })
+
+              //Shortcut to reload the page.
+              globalShortcut.register('CmdOrCtrl+R', () => {
+                  mainWindow.webContents.reload();
+              })
+              globalShortcut.register('CmdOrCtrl+Left', () => {
+                  mainWindow.webContents.goBack();
+                  mainWindow.webContents.reload();
+              })
+
+              mainWindow.on('app-command', (e, cmd) => {
+                  // Navigate the window back when the user hits their mouse back button
+                  if (cmd === 'browser-backward' && mainWindow.webContents.canGoBack()) {
+                      mainWindow.webContents.goBack()
+                  }
+              })
     })
 })
 app.on('window-all-closed', function() {
@@ -57,7 +117,5 @@ app.on('window-all-closed', function() {
     }
 })
 app.on('activate', function() {
-    if (mainWindow === null) {
-        createWindow()
-    }
+    mainWindow.show();
 })
